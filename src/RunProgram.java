f1caf835a76f633456017f025615273ba68d5c41
RHONT <rg915i@yandex.ru>
2024-01-08 21:49:43 +0300

    Тесты готовы

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -1,12 +1,14 @@
+import worktask.Warperr_Array;
+
 import java.io.*;
 import java.util.Arrays;
-import java.util.Random;
+
 
 public class RunProgram {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         short[] input = {1, 22, 10, 65, 66, 200, 300,1,126,127,128,129,31,32,33};
-        Warp_Array warp = new Warp_Array(input);
+        Warperr_Array warp = new Warperr_Array(input);
         System.out.println("Массив до сериализации");
         System.out.println(Arrays.toString(warp.getArray()));
 
@@ -19,7 +21,7 @@ public class RunProgram {
 
         FileInputStream fileIn = new FileInputStream("demo.txt");
         ObjectInputStream ois = new ObjectInputStream(fileIn);
-        Warp_Array deserializedArray = (Warp_Array) ois.readObject();
+        Warperr_Array deserializedArray = (Warperr_Array) ois.readObject();
         ois.close();
         fileIn.close();
 
@@ -28,8 +30,7 @@ public class RunProgram {
 
 
 
-        System.out.println("Размер массива: "+warp.getArray().length*16 + " Бит");
-        System.out.println("Размер строки: "+warp.getStr().length()*8 + " Бит");
+
 
     }
 }
